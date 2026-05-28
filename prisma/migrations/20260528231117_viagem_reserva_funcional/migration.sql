7a8d50e624da753ff4cef1e5e5657fe3a25e161c
GuilhermeMaglia <guipmagliad@gmail.com>
2026-05-28 20:29:29 -0300

    🐛 Tables
    Consertado Relacionamentos Entre Tabelas

--- prisma/migrations/20260528231117_viagem_reserva_funcional/migration.sql
+++ prisma/migrations/20260528231117_viagem_reserva_funcional/migration.sql
@@ -11,16 +11,14 @@ CREATE TABLE `clientes` (
 -- CreateTable
 CREATE TABLE `viagens` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `idCliente` INTEGER NOT NULL,
-    `reservaid` INTEGER NOT NULL,
     `destino` VARCHAR(60) NOT NULL,
     `transporte` ENUM('Aereo', 'Maritmo', 'Terrestre') NOT NULL DEFAULT 'Terrestre',
     `dataSaida` DATETIME(3) NOT NULL,
     `dataRetorno` DATETIME(3) NOT NULL,
     `roteiro` TEXT NULL,
     `localSaida` TEXT NULL,
-    `Nvagas` DECIMAL(65, 30) NOT NULL,
-    `Nreservas` DECIMAL(65, 30) NOT NULL,
+    `Nvagas` INTEGER NOT NULL,
+    `Nreservas` INTEGER NOT NULL DEFAULT 0,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -28,7 +26,8 @@ CREATE TABLE `viagens` (
 -- CreateTable
 CREATE TABLE `reservas` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `clienteid` INTEGER NOT NULL,
+    `clienteId` INTEGER NOT NULL,
+    `viagemId` INTEGER NOT NULL,
     `pacote` ENUM('Aereo', 'Turistico', 'Hospedagem', 'Experiência', 'SeguroViagem') NOT NULL,
     `preco` DECIMAL(9, 2) NOT NULL,
 
@@ -36,10 +35,7 @@ CREATE TABLE `reservas` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `viagens` ADD CONSTRAINT `viagens_idCliente_fkey` FOREIGN KEY (`idCliente`) REFERENCES `clientes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `reservas` ADD CONSTRAINT `reservas_clienteId_fkey` FOREIGN KEY (`clienteId`) REFERENCES `clientes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `viagens` ADD CONSTRAINT `viagens_reservaid_fkey` FOREIGN KEY (`reservaid`) REFERENCES `reservas`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `reservas` ADD CONSTRAINT `reservas_clienteid_fkey` FOREIGN KEY (`clienteid`) REFERENCES `clientes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `reservas` ADD CONSTRAINT `reservas_viagemId_fkey` FOREIGN KEY (`viagemId`) REFERENCES `viagens`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
